7f4abb7679ecfc35ccb752be8768e36ed5fbf9bb
nttlinh1809 <nttlinh.el@gmail.com>
2022-09-15 18:02:31 +0700

    add login

--- src/utilities/Authentication.java
+++ src/utilities/Authentication.java
@@ -4,13 +4,12 @@
  */
 package utilities;
 
-import entities.User;
+//import entities.User;
+import jakarta.servlet.http.Cookie;
 import java.util.Date;
 import java.util.HashMap;
 //import models.UserModel;
 import org.json.JSONObject;
-
-//import javax.servlet.http.Cookie;
 
 /**
  *
@@ -18,28 +17,28 @@ import org.json.JSONObject;
  */
 public class Authentication {
 
-//    public static String getTokenFromCookies(Object cookies) {
-//        for (Cookie cookie : (Cookie[])cookies) {
-//            if (cookie.getName().equals("token")) {
-//                return cookie.getValue();
-//            }
-//        }
-//        return null;
-//    }
-//
-//    public static Cookie createTokenCookie(Long uid, Integer expiryTime) {
-//        HashMap<String, Object> data = new HashMap<>();
-//
-//        data.put("uid", uid);
-//        data.put("expiry", new Date().getTime() + expiryTime * 1000);
-//
-//        String token = TokenGenerator.generate(data, GlobalConstants.AUTH_SECRET_KEY);
-//
-//        Cookie c = new Cookie("token", token);
-//        c.setMaxAge(expiryTime);
-//
-//        return c;
-//    }
+    public static String getTokenFromCookies(Object cookies) {
+        for (Cookie cookie : (Cookie[])cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    public static Cookie createTokenCookie(Long uid, Integer expiryTime) {
+        HashMap<String, Object> data = new HashMap<>();
+
+        data.put("uid", uid);
+        data.put("expiry", new Date().getTime() + expiryTime * 1000);
+
+        String token = TokenGenerator.generate(data, GlobalConstants.AUTH_SECRET_KEY);
+
+        Cookie c = new Cookie("token", token);
+        c.setMaxAge(expiryTime);
+
+        return c;
+    }
 
 //    public static User getUserInformationByToken(String token) throws Exception {
 //        if (!TokenGenerator.validCheck(token, GlobalConstants.AUTH_SECRET_KEY)) {
