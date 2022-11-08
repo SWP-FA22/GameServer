26190568e86a2429b2e7bd3003f671ceb510f787
Huupb <banghuund99@gmail.com>
2022-11-09 01:02:45 +0700

    fix daily checkin

--- src/java/utilities/StatusCheckIn.java
+++ src/java/utilities/StatusCheckIn.java
@@ -1,49 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package utilities;
 
 import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import models.PlayerModel;
-
-/**
- *
- * @author Huu
- */
 public class StatusCheckIn {
-     static {
-        Thread thread1 = new Thread(() -> {
-            Date today = new Date();
-            Date nextDay = new Date(today.getYear(), today.getMonth(), today.getDate());
-            nextDay = new Date(nextDay.getTime() + 1000 * 60 * 60 * 24);
 
-            while (true) {
+    public static void CheckIn() {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        Runnable cancelUnpaidOrdersTask = new Runnable() {
+            Date today = null, nextDay = null;
+
+            public void init() {
+                today = new Date();
+                nextDay = new Date(today.getYear(), today.getMonth(), today.getDate());
+                nextDay = new Date(nextDay.getTime() + 1000 * 60 * 60 * 24);
+            }
+
+            public void run() {
+                if (nextDay == null) {
+                    init();
+                }
+
                 today = new Date();
 
                 if (today.after(nextDay)) {
                     try {
                         System.out.println("Reset diem danh");
-                        PlayerModel pm=new PlayerModel();
+                        PlayerModel pm = new PlayerModel();
                         pm.resetCheckin();
                         nextDay = new Date(today.getYear(), today.getMonth(), today.getDate());
                         nextDay = new Date(nextDay.getTime() + 1000 * 60 * 60 * 24);
                     } catch (Exception ex) {
                         Logger.getLogger(StatusCheckIn.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    
-                }
 
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    
                 }
             }
-        });
+        };
 
-        thread1.run();
+        scheduler.scheduleAtFixedRate(cancelUnpaidOrdersTask, 0, 1, TimeUnit.SECONDS);
     }
 }
